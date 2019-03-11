fdbadfd475bee716652aa63ea9849e65cf01d841
SahibSinghBanga <sahib_singh_1997@hotmail.com>
2019-03-11 16:21:16 +0530

    compose store

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
-// import { composeWithDevTools } from 'redux-devtools-extension';
 
 const initialState = {};
 
@@ -9,6 +8,6 @@ const middleware = [thunk];
 
 const store = createStore(rootReducer, initialState,
     compose(applyMiddleware(...middleware),
-        (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))) || compose;
+        (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())));
 
 export default store;
