eecd187f964eeb2fe2f557a74601447e3d9f2794
SahibSinghBanga <sahib_singh_1997@hotmail.com>
2019-03-11 15:06:22 +0530

    Mobile Fix

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default () => {
   return (
     <div>
-      <h1 className="display-4"><span className="text-primary">About</span> Contact Manager</h1>
+      <h1 className="display-4">
+        <span className="text-primary">About </span>
+        Contact
+      <span className="text-primary"> Manager</span>
+      </h1>
       <p className="lead">Simple app to manage contacts</p>
       <p className="text-secondary">Version 1.0.0</p>
     </div>
